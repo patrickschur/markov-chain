0978adf1870b43b298f7049e2fabb6a18b7d29f3
Patrick Schur <patrick_schur@outlook.de>
2017-01-14 08:47:45 +0100

    updated and added phpunit tests

--- README.md
+++ README.md
@@ -1,3 +1,58 @@
 # markov-chain
 
 An implementation of the Markov chain algorithm in PHP.
+
+Installation with Composer
+-
+```bash
+$ composer require patrickschur/markov-chain
+```
+
+How to use
+-
+WordTokenizer
+-
+```php
+use MarkovChain\MarkovChain;
+use MarkovChain\Tokenizer\WordTokenizer;
+ 
+$c = new MarkovChain(new WordTokenizer());
+ 
+$c->learn([
+    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+    'At vero eos et accusam et justo duo dolores et ea rebum.',
+]);
+ 
+print_r($c->classify('et'));
+/*
+    [
+        'accusam' => 0.33333333333333,
+        'justo' => 0.33333333333333,
+        'ea' => 0.33333333333333
+    ]
+*/
+```
+
+CharTokenizer
+-
+```php
+use MarkovChain\MarkovChain;
+use MarkovChain\Tokenizer\CharTokenizer;
+
+$c = new MarkovChain(new CharTokenizer());
+ 
+$c->learn([
+    'Lorem ipsum dolor sit amet, consetetur sadipscing elitr.',
+    'At vero eos et accusam et justo duo dolores et ea rebum.',
+]);
+ 
+print_r($c->classify('a'));
+/*
+    [
+        'm' => 0.4,
+        'd' => 0.2,
+        'c' => 0.2,
+        'r' => 0.2
+    ]
+*/
+```
---
+++ tests/MarkovChainTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace MarkovChain\Tests;
+
+use MarkovChain\MarkovChain;
+use MarkovChain\Tokenizer\CharTokenizer;
+use MarkovChain\Tokenizer\WordTokenizer;
+
+class MarkovChainTest extends \PHPUnit_Framework_TestCase
+{
+    public function testWordTokenizer()
+    {
+        $c = new MarkovChain(new WordTokenizer());
+
+        $c->learn([
+            'a b c d e f g',
+            'g e c f a b d',
+            'g f e b g a d',
+        ]);
+
+        $this->assertEquals(['b' => 0.6666666666666667, 'd' => 0.3333333333333333], $c->classify('a'));
+    }
+
+    public function testCharTokenizer()
+    {
+        $c = new MarkovChain(new CharTokenizer());
+
+        $c->learn([
+            'abaabbaaabbb',
+            'babbaabbbaaa',
+            'cdccddcccddd',
+            'dcddccdddccc',
+        ]);
+
+        $this->assertEquals($c->classify('a')['a'], $c->classify('b')['b']);
+    }
+
+    public function testIsNull()
+    {
+        $c = new MarkovChain(new WordTokenizer());
+
+        $c->learn([
+            '',
+            '',
+        ]);
+
+        $this->assertNull($c->classify('a'));
+    }
+}
